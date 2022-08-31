516ee2843cf646b28e94bd1dff40edf75684f2f6
Indra Azimi <indraazimi@yahoo.com>
2022-08-31 10:01:57 +0700

    Menambahkan aksi lanjut

--- app/src/main/java/com/indraazimi/helloworld/MainActivity.kt
+++ app/src/main/java/com/indraazimi/helloworld/MainActivity.kt
@@ -2,6 +2,7 @@ package com.indraazimi.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
     private lateinit var textView: TextView
+    private var index = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +27,12 @@ class MainActivity : AppCompatActivity() {
         imageView = findViewById(R.id.imageView)
         textView = findViewById(R.id.textView)
         showData(data[0])
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            index = (index + 1) % data.size
+            showData(data[index])
+        }
     }
 
     private fun showData(hewan: Hewan) {
