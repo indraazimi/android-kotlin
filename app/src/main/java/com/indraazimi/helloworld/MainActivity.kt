de73c37373f4878a235e92def648fbb867789cf5
Indra Azimi <indraazimi@yahoo.com>
2022-08-31 14:16:53 +0700

    Menghitung BMI dan kategorinya

--- app/src/main/java/com/indraazimi/helloworld/MainActivity.kt
+++ app/src/main/java/com/indraazimi/helloworld/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.indraazimi.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.indraazimi.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +15,35 @@ class MainActivity : AppCompatActivity() {
 
         binding.button.setOnClickListener { hitung() }
     }
-    
+
     private fun hitung() {
-        Log.d("MainActivity", "Tombol berhasil diklik!")
+        val berat = binding.beratEditText.text.toString().toFloat()
+        val tinggi = binding.tinggiEditText.text.toString().toFloat() / 100
+        val bmi = berat / (tinggi * tinggi)
+
+        val selectedId = binding.radioGroup.checkedRadioButtonId
+        val isMale = selectedId == R.id.priaRadioButton
+        val kategori = getKategori(bmi, isMale)
+
+        binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
+        binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
+    }
+
+    private fun getKategori(bmi: Float, isMale: Boolean): String {
+        val stringRes = if (isMale) {
+            when {
+                bmi < 20.5 -> R.string.kurus
+                bmi >= 27.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        }
+        else {
+            when {
+                bmi < 18.5 -> R.string.kurus
+                bmi >= 25.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        }
+        return getString(stringRes)
     }
 }
