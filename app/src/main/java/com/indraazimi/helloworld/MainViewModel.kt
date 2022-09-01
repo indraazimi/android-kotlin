570a78823f614ba39e9bddc64cbaea14c7810e8a
Indra Azimi <indraazimi@yahoo.com>
2022-09-01 14:22:49 +0700

    Mengambil data dari internet

--- app/src/main/java/com/indraazimi/helloworld/MainViewModel.kt
+++ app/src/main/java/com/indraazimi/helloworld/MainViewModel.kt
@@ -1,8 +1,12 @@
 package com.indraazimi.helloworld
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
@@ -10,6 +14,18 @@ class MainViewModel : ViewModel() {
 
     init {
         data.value = initData()
+        retrieveDataFromServer()
+    }
+
+    private fun retrieveDataFromServer() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val result = HewanApi.service.getHewan()
+                Log.d("MainViewModel", result)
+            } catch (e: Exception) {
+                Log.d("MainViewModel", "Gagal: ${e.message}")
+            }
+        }
     }
 
     fun getData(): LiveData<List<Hewan>> = data
