a8897217f7b552c1a2f657ebe2110b5c120cc717
Indra Azimi <indraazimi@yahoo.com>
2022-09-02 09:43:36 +0700

    Navigasi tampilan main ke detail

--- app/src/main/java/com/indraazimi/helloworld/MainActivity.kt
+++ app/src/main/java/com/indraazimi/helloworld/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.indraazimi.helloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.indraazimi.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
